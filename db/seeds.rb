ee4d70ba198a4d522717fb5283c7ca45825b6e5a
Wen Xiao <wennstyle@gmail.com>
2022-08-07 21:03:43 -0400

    seeded test data success

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+archery = Activity.create(activity_name: 'Archery', price: 29, activity_timeslot: "9:00am-11:00am,11:30am-1:30pm,2:00pm-4:00pm,4:30pm-6:30pm")
+
+
+
+
 
 puts "✅ Done seeding!"
