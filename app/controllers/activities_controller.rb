07debfb79f6ab846a47d3c37c53444ec77d4066a
Wen Xiao <wennstyle@gmail.com>
2022-08-08 01:00:14 -0400

    added additional seeding data

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < Sinatra::Base
     set :default_content_type, 'application/json'
 
     get "/activities" do
-        { message: "Good luck with your project!" }.to_json
+        Activity.all.to_json
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 archery = Activity.create(activity_name: 'Archery', price: 29, activity_timeslot: "9:00am-11:00am,11:30am-1:30pm,2:00pm-4:00pm,4:30pm-6:30pm")
-
+paintball = Activity.create(activity_name: 'Paintball', price: 32, activity_timeslot: "9:00am-10:30am,11:30am-12:00pm,1:00pm-2:30pm,3:30pm-5:00pm")
 
 
 
