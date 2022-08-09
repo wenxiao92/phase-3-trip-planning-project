a9cde50cef819fc4e854e62bbe06df8b3f5636f5
Wen Xiao <wennstyle@gmail.com>
2022-08-08 22:44:13 -0400

    added more activities to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,9 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 archery = Activity.create(activity_name: 'Archery', price: 29, activity_timeslot: "9:00am-11:00am,11:30am-1:30pm,2:00pm-4:00pm,4:30pm-6:30pm")
 paintball = Activity.create(activity_name: 'Paintball', price: 32, activity_timeslot: "9:00am-10:30am,11:30am-12:00pm,1:00pm-2:30pm,3:30pm-5:00pm")
-
-
-
+rafting = Activity.create(activity_name: 'Rafting (whitewater)', price: 55, activity_timeslot: "9:00am-3:00pm,10:00am-4:00pm")
+canoe = Activity.create(activity_name: 'Canoe', price: 20, activity_timeslot: "9:00am-10:00am,10:15am-11:15pm,11:30am-12:30pm,1:30pm-2:30pm,2:45pm-3:45pm,4:00pm-5:00pm")
+axe_throwing = Activity.create(activity_name: 'Axe Throwing', price: 25, activity_timeslot: "9:00am-10:00am,10:15am-11:15pm,11:30am-12:30pm,1:30pm-2:30pm,2:45pm-3:45pm,4:00pm-5:00pm")
+horseback_riding = Activity.create(activity_name: 'Horseback Riding', price: 50, activity_timeslot: "9:00am-10:00am,10:15am-11:15pm,11:30am-12:30pm,1:30pm-2:30pm,2:45pm-3:45pm,4:00pm-5:00pm")
 
 puts "✅ Done seeding!"
