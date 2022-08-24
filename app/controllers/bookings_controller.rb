a99e7bb10d87a5ac3a70c79b214db193d8f9053d
Wen Xiao <wennstyle@gmail.com>
2022-08-24 02:44:31 -0400

    added booking route based on activity

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,5 +5,23 @@ class BookingsController < ActivitiesController
     get "/bookings" do
       Booking.all.to_json
     end
-  
-  end
+
+    # get "/bookings/revised" do
+    # #   # activityLength = Activity.all.length
+    # #   # revisedBooking = Booking.all.select {|v| v.activity_id === } #gets all activity in a loop from 1-6
+    # #   # revisedBook.to_json
+    #   test = Booking.find_each.select {|v| v.activity_id === }
+
+    #   end
+    #   test.to_json
+        
+    # end
+
+    get "/bookings/:activity_id" do
+      # test = Booking.find(params[:activity_id])
+      # test.to_json
+      test = Booking.where(activity_id: params[:activity_id])
+      test.to_json
+    end
+
+end
